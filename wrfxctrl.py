58dab026573fa0d02089a40a2113ca70f48e5449
Jan Mandel <jan.mandel@gmail.com>
2016-09-23 22:40:17 -0600

    fixing strings

--- wrfxctrl.py
+++ wrfxctrl.py
@@ -45,6 +45,7 @@ host = conf['host']
 debug = conf['debug'] in ['T' 'True' 't' 'true']
 port=conf['port']
 urls = {'submit': root+'/submit', 'welcome': root+'/start', 'overview': root+'/overview'}
+print ( ' * Listening at %s' % urls['welcome'] )
 
 app = Flask(__name__)
 

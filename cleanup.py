11851cf49cc40371d0e5b2bd61b24d2a82f851fc
Jan Mandel <jan.mandel@gmail.com`>
2016-11-01 14:01:43 -0600

    making cleanup callable

--- cleanup.py
+++ cleanup.py
@@ -3,17 +3,11 @@ import json
 import sys
 import logging
 
-if len(sys.argv) < 2:
-    print('usage: %s [list|delete <name>' % sys.argv[0])
-    sys.exit(1)
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.info('WRFXCTRL %s' % sys.argv[0])
 conf = json.load(open('etc/conf.json'))
 sims_path = conf['sims_path']
-simulations = load_simulations(sims_path)
-if sys.argv[1] == 'delete':
-    sim_id = sys.argv[2]
+
+def cleanup_delete(sim_id):
+    simulations = load_simulations(sims_path)
     try:
         logging.info('Deleting simulation %s' % sim_id)
         sim_info = simulations[sim_id]
@@ -21,10 +15,27 @@ if sys.argv[1] == 'delete':
     except KeyError:
         logging.error('Simulation %s not found.' % sim_id)
         delete_simulation_files(sim_id,conf) # rm any stray files
-elif sys.argv[1] == 'list':
+
+def cleanup_list():
+    simulations = load_simulations(sims_path)
     print('%-30s desc' % 'id') 
     print('-' * 40)
     for k in sorted(simulations):
         print('%-30s %s' % (k, simulations[k]['description']))
-else:
-    logging.error('command line not understood %s' % sys.argv)
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        print('usage: %s [list|delete <name>' % sys.argv[0])
+        sys.exit(1)
+
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    if sys.argv[1] == 'delete':
+        sim_id = sys.argv[2]
+        cleanup_delete(sim_id)
+    elif sys.argv[1] == 'list':
+        cleanup_list()
+    else:
+        logging.error('command line not understood %s' % sys.argv)
+

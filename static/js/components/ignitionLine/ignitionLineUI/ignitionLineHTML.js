fcd415ab66c54ed67d5458af0995487925175a3d
Chase Porter <chaseporter@Chases-MacBook-Pro-2.local>
2023-04-18 15:13:49 -0700

    Adds start and end time selectors

--- static/js/components/ignitionLine/ignitionLineUI/ignitionLineHTML.js
+++ static/js/components/ignitionLine/ignitionLineUI/ignitionLineHTML.js
@@ -1,6 +1,20 @@
 export const ignitionLineHTML = `
     <div id="ignition-line-component">
         <h3>Ignition Line</h3>
+        <div id="start-dates" class="field">
+            <div class="ui input left icon">
+                <i class="calendar icon"></i>
+                <input name="ignition_start" id="start-time-input" type="text" placeholder="YYYY-MM-DD_HH:MM:SS">
+            </div>
+            <span id="ignition-time-warning" class="not-valid-warning">The ignition time must be between 1/1/1979 and now in the format YYYY-MM-DD_HH:MM:SS</span>
+        </div>
+        <div id="end-dates" class="field">
+            <div class="ui input left icon">
+                <i class="calendar icon"></i>
+                <input name="ignition_end" id="end-time-input" type="text" placeholder="YYYY-MM-DD_HH:MM:SS">
+            </div>
+            <span id="ignition-time-warning" class="not-valid-warning">The ignition time must be between 1/1/1979 and now in the format YYYY-MM-DD_HH:MM:SS</span>
+        </div>
         <div id="ignition-line-markers"></div> 
     </div>
 `;
--- static/js/components/ignitionLine/ignitionLineUI/ignitionLineUI.js
+++ static/js/components/ignitionLine/ignitionLineUI/ignitionLineUI.js
@@ -7,6 +7,8 @@ export class IgnitionLineUI extends AppStateSubscriber {
         super();
         this.innerHTML = ignitionLineHTML;
         this.uiElements = {
+            ignitionStartUI: this.querySelector('#start-time-input'),
+            ignitionEndUI: this.querySelector('#end-time-input'),
             ignitionLineComponentUI: this.querySelector('#ignition-line-component'),
             ignitionLineMarkersListUI: this.querySelector('#ignition-line-markers'),
         };
@@ -14,6 +16,8 @@ export class IgnitionLineUI extends AppStateSubscriber {
 
     connectedCallback() {
         this.setVisibilityFromAppState();
+        $('#start-time-input').datetimepicker({ value: moment().utc(), formatTime: 'h:mm a', formatDate: 'm.d.Y', step:15 });
+        $('#end-time-input').datetimepicker({ value: moment().utc(), formatTime: 'h:mm a', formatDate: 'm.d.Y', step:15 });
     }
 
     isVisible() {
@@ -26,6 +30,18 @@ export class IgnitionLineUI extends AppStateSubscriber {
         if (!this.isVisible()) {
             ignitionLineComponentUI.classList.remove('hidden');
         }
+    }
+
+    disableStartAndEndTimes() {
+        let { ignitionStartUI, ignitionEndUI } = this.uiElements;
+        ignitionStartUI.disabled = true;
+        ignitionEndUI.disabled = true;
+    }
+
+    enableStartAndEndTimes() {
+        let { ignitionStartUI, ignitionEndUI } = this.uiElements;
+        ignitionStartUI.disabled = false;
+        ignitionEndUI.disabled = false;
     }
 
     hideComponent() {

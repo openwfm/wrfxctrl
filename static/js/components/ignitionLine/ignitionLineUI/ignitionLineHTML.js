70e40c3b626122f25432d4000a730a029021538d
Chase Porter <chaseporter@Chases-MacBook-Pro-2.local>
2023-05-25 21:59:21 -0700

    Formatting the layout of the time selectors

--- static/js/components/ignitionLine/ignitionLineUI/ignitionLineHTML.js
+++ static/js/components/ignitionLine/ignitionLineUI/ignitionLineHTML.js
@@ -5,7 +5,7 @@ export const ignitionLineHTML = `
           <label>Split times evenly between start and end</label>
           <input type="checkbox" id="start-end-checkbox" value="split-start-end">
         </div>
-        <div id="ignition-times">
+        <div id="ignition-times" class="two fields" style="margin-bottom: 15px">
           <div id="start-dates" class="field">
             <label>First Ignition</label>
             <div class="ui input left icon">
@@ -15,11 +15,11 @@ export const ignitionLineHTML = `
             <span id="ignition-time-warning" class="not-valid-warning">The ignition time must be between 1/1/1979 and now in the format YYYY-MM-DD_HH:MM:SS</span>
           </div>
           <div id="end-dates" class="field">
-          <label>End Ignition</label>
-          <div class="ui input left icon">
-            <i class="calendar icon"></i>
-            <input name="ignition_end" id="end-ignition-input" type="text" placeholder="YYYY-MM-DD_HH:MM:SS">
-          </div>
+            <label>End Ignition</label>
+            <div class="ui input left icon">
+              <i class="calendar icon"></i>
+              <input name="ignition_end" id="end-ignition-input" type="text" placeholder="YYYY-MM-DD_HH:MM:SS">
+            </div>
             <span id="ignition-time-warning" class="not-valid-warning">The ignition time must be between 1/1/1979 and now in the format YYYY-MM-DD_HH:MM:SS</span>
           </div>
         </div>

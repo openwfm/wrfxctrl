3f0d5be3fa607f797c177f77e9c699b6f04c1cc9
Chase Porter <chaseporter@Chases-MacBook-Pro-2.local>
2023-06-06 14:29:22 -0700

    Fixes display of simulation start and end times

--- static/js/components/ignitionLine/ignitionLineUI/ignitionLineHTML.js
+++ static/js/components/ignitionLine/ignitionLineUI/ignitionLineHTML.js
@@ -5,22 +5,24 @@ export const ignitionLineHTML = `
           <label>Split times evenly between start and end</label>
           <input type="checkbox" id="start-end-checkbox" value="split-start-end">
         </div>
-        <div id="ignition-times" class="two fields" style="margin-bottom: 15px">
-          <div id="start-dates" class="field">
-            <label>First Ignition</label>
-            <div class="ui input left icon">
+        <div id="ignition-times">
+          <div class="two fields" style="margin-bottom: 15px">
+            <div id="start-dates" class="field">
+              <label>First Ignition</label>
+              <div class="ui input left icon">
+                  <i class="calendar icon"></i>
+                  <input name="ignition_start" id="start-ignition-input" type="text" placeholder="YYYY-MM-DD_HH:MM:SS">
+              </div>
+              <span id="ignition-time-warning" class="not-valid-warning">The ignition time must be between 1/1/1979 and now in the format YYYY-MM-DD_HH:MM:SS</span>
+            </div>
+            <div id="end-dates" class="field">
+              <label>End Ignition</label>
+              <div class="ui input left icon">
                 <i class="calendar icon"></i>
-                <input name="ignition_start" id="start-ignition-input" type="text" placeholder="YYYY-MM-DD_HH:MM:SS">
+                <input name="ignition_end" id="end-ignition-input" type="text" placeholder="YYYY-MM-DD_HH:MM:SS">
+              </div>
+              <span id="ignition-time-warning" class="not-valid-warning">The ignition time must be between 1/1/1979 and now in the format YYYY-MM-DD_HH:MM:SS</span>
             </div>
-            <span id="ignition-time-warning" class="not-valid-warning">The ignition time must be between 1/1/1979 and now in the format YYYY-MM-DD_HH:MM:SS</span>
-          </div>
-          <div id="end-dates" class="field">
-            <label>End Ignition</label>
-            <div class="ui input left icon">
-              <i class="calendar icon"></i>
-              <input name="ignition_end" id="end-ignition-input" type="text" placeholder="YYYY-MM-DD_HH:MM:SS">
-            </div>
-            <span id="ignition-time-warning" class="not-valid-warning">The ignition time must be between 1/1/1979 and now in the format YYYY-MM-DD_HH:MM:SS</span>
           </div>
         </div>
         <div id="ignition-line-markers" class="ignition-markers"></div> 
